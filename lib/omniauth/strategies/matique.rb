3e6dc3c36b0ee22bac5fce8756dddd8dcd6fc0b8
matique <dittmar.krall@matique.de>
2016-02-07 14:15:04 +0100

    'some tests running'

--- lib/omniauth/strategies/matique.rb
+++ lib/omniauth/strategies/matique.rb
@@ -4,6 +4,15 @@ module OmniAuth
   module Strategies
     class Matique < OmniAuth::Strategies::OAuth2
 
+      option :client_options, {
+	site: 'https://lvh.me',
+	authorize_url: 'https//lvh.me/login/oauth/authorize',
+	token_url: 'https://lvh.me/login/oauth/access_token'
+      }
+
+
+
+
     end
   end
 end
