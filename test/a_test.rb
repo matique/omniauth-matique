50727979dc0d0b11e2fd05be5cfb770fb86744ab
matique <dittmar.krall@matique.de>
2016-02-08 12:24:45 +0100

    '...'

--- test/a_test.rb
+++ test/a_test.rb
@@ -8,15 +8,15 @@ class StrategyTest < StrategyTestCase
 end
 
 describe OmniAuth::Strategies::Matique, 'raw_info test' do
-  let(:strategy) { OmniAuth::Strategies::Matique.new({}) }
+  let(:strategy)     { OmniAuth::Strategies::Matique.new({}) }
+  let(:access_token) { stub('OAuth2::AccessToken') }
 
   def setup
-    @access_token = stub('OAuth2::AccessToken')
-    strategy.stubs(:access_token).returns(@access_token)
+    strategy.stubs(:access_token).returns(access_token)
   end
 
   it 'performs a GET to https://lvh.me' do
-    @access_token.expects(:get).with('info').
+    access_token.expects(:get).with('info').
       returns(stub_everything('OAuth2::Response'))
     strategy.raw_info
   end
@@ -27,14 +27,14 @@ describe OmniAuth::Strategies::Matique, 'raw_info test' do
     raw_response.stubs(:status).returns(200)
     raw_response.stubs(:headers).returns({'Content-Type' => 'application/json' })
     oauth2_response = OAuth2::Response.new(raw_response)
-    @access_token.stubs(:get).with('info').returns(oauth2_response)
+    access_token.stubs(:get).with('info').returns(oauth2_response)
     assert_kind_of Hash, strategy.raw_info
     assert_equal 'thar', strategy.raw_info['ohai']
   end
 
   it 'returns an empty hash when the response is false' do
     oauth2_response = stub('OAuth2::Response', :parsed => false)
-    @access_token.stubs(:get).with('info').returns(oauth2_response)
+    access_token.stubs(:get).with('info').returns(oauth2_response)
     assert_kind_of Hash, strategy.raw_info
   end
 
