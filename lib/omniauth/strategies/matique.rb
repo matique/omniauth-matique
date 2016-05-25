a8d66cb7a9d4885ac4d0452486def4abd5bd911e
matique <dittmar.krall@matique.de>
2016-05-25 11:14:25 +0200

    'Rails 5 related'

--- lib/omniauth/strategies/matique.rb
+++ lib/omniauth/strategies/matique.rb
@@ -3,21 +3,17 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Matique < OmniAuth::Strategies::OAuth2
-#      SITE = 'https://lvh.me'
-      SITE = 'http://localhost:3010'
-      STRATEGY = 'matique'
-#      STRATEGY = 'sso'
+      SITE = 'https://lvh.me'
 
       option :client_options, {
 	site: SITE,
-	authorize_url: "#{SITE}/auth/#{STRATEGY}/authorize",
-	token_url: "#{SITE}/auth/#{STRATEGY}/access_token"
+	authorize_url: "#{SITE}/auth/matique/authorize",
+	token_url: "#{SITE}/auth/matique/access_token"
       }
 
       uid { raw_info['id'] }
 
       info do
-#        { 'email' => raw_info['info']['email'] }
 	{ 'email' => raw_info['email'] }
       end
 
@@ -26,9 +22,9 @@ module OmniAuth
       end
 
       def raw_info
-#        @raw_info ||= access_token.get('user').parsed || {}
-	@raw_info ||= access_token.get(
- "/auth/matique/user.json?oauth_token=#{access_token.token}").parsed
+#        @raw_info ||= access_token.get('info').parsed || {}
+       @raw_info ||= access_token.get(
+ "/auth/matique/user.json?oauth_token=#{access_token.token}").parsed || {}
       end
 
     end
--- test/a_test.rb
+++ test/a_test.rb
@@ -10,14 +10,16 @@ end
 describe OmniAuth::Strategies::Matique, 'raw_info test' do
   let(:strategy)     { OmniAuth::Strategies::Matique.new({}) }
   let(:access_token) { stub('OAuth2::AccessToken') }
+  let(:get_param)    { '/auth/matique/user.json?oauth_token=123' }
 
   def setup
     super
     strategy.stubs(:access_token).returns(access_token)
+    access_token.stubs(:token).returns(123)
   end
 
   it 'performs a GET to https://lvh.me' do
-    access_token.expects(:get).with('info').
+    access_token.expects(:get).with(get_param).
       returns(stub_everything('OAuth2::Response'))
     strategy.raw_info
   end
@@ -28,14 +30,14 @@ describe OmniAuth::Strategies::Matique, 'raw_info test' do
     raw_response.stubs(:status).returns(200)
     raw_response.stubs(:headers).returns({'Content-Type' => 'application/json' })
     oauth2_response = OAuth2::Response.new(raw_response)
-    access_token.stubs(:get).with('info').returns(oauth2_response)
+    access_token.stubs(:get).with(get_param).returns(oauth2_response)
     assert_kind_of Hash, strategy.raw_info
     assert_equal 'thar', strategy.raw_info['ohai']
   end
 
   it 'returns an empty hash when the response is false' do
     oauth2_response = stub('OAuth2::Response', :parsed => false)
-    access_token.stubs(:get).with('info').returns(oauth2_response)
+    access_token.stubs(:get).with(get_param).returns(oauth2_response)
     assert_kind_of Hash, strategy.raw_info
   end
 
