ff7d059c3e810e0ece1717913fc2182bcd3aed33
matique <dittmar.krall@matique.de>
2016-06-20 12:42:44 +0200

    'cleaning'

--- test/raw_info_test.rb
+++ test/raw_info_test.rb
@@ -20,29 +20,26 @@ describe OmniAuth::Strategies::Matique, 'raw_info test' do
 
   it 'performs a GET to https://...' do
     access_token.expects(:get).with(get_param).
-#    access_token.expects(:get).with('info').
       returns(stub_everything('OAuth2::Response'))
     strategy.raw_info
   end
 
-#  it 'returns a Hash' do
-#    raw_response = stub('Faraday::Response')
-#    raw_response.stubs(:body).returns('{ "ohai": "thar" }')
-#    raw_response.stubs(:status).returns(200)
-#    raw_response.stubs(:headers).returns({'Content-Type' => 'application/json' })
-#    oauth2_response = OAuth2::Response.new(raw_response)
-#    access_token.stubs(:get).with(get_param).returns(oauth2_response)
-##    access_token.stubs(:get).with('info').returns(oauth2_response)
-#    assert_kind_of Hash, strategy.raw_info
-#    assert_equal 'thar', strategy.raw_info['ohai']
-#  end
-#
-#  it 'returns an empty hash when the response is false' do
-#    oauth2_response = stub('OAuth2::Response', :parsed => false)
-#    access_token.stubs(:get).with(get_param).returns(oauth2_response)
-##    access_token.stubs(:get).with('info').returns(oauth2_response)
-#    access_token.stubs(:get).with(get_param).returns(oauth2_response)
-#    assert_kind_of Hash, strategy.raw_info
-#  end
+  it 'returns a Hash' do
+    raw_response = stub('Faraday::Response')
+    raw_response.stubs(:body).returns('{ "ohai": "thar" }')
+    raw_response.stubs(:status).returns(200)
+    raw_response.stubs(:headers).returns({'Content-Type' => 'application/json' })
+    oauth2_response = OAuth2::Response.new(raw_response)
+    access_token.stubs(:get).with(get_param).returns(oauth2_response)
+    assert_kind_of Hash, strategy.raw_info
+    assert_equal 'thar', strategy.raw_info['ohai']
+  end
+
+  it 'returns an empty hash when the response is false' do
+    oauth2_response = stub('OAuth2::Response', :parsed => false)
+    access_token.stubs(:get).with(get_param).returns(oauth2_response)
+    access_token.stubs(:get).with(get_param).returns(oauth2_response)
+    assert_kind_of Hash, strategy.raw_info
+  end
 
 end
