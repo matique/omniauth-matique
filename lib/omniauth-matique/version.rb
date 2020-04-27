35c6888eaf8e3aeedfb4c34d71c2b23251bbaf74
matique <dittmar.krall@matique.de>
2020-04-27 12:43:07 +0200

    'v0.1.6'

--- lib/omniauth-matique/version.rb
+++ lib/omniauth-matique/version.rb
@@ -2,7 +2,8 @@
 
 module OmniAuth
   module Matique
-    VERSION = '0.1.5'   # 2019-10-06
+    VERSION = '0.1.6'   # 2020-04-27
+#   VERSION = '0.1.5'   # 2019-10-06
 #   VERSION = '0.1.4'   # 2019-03-02
 #   VERSION = '0.1.3'
   end
