0471cd5cda180f386b8eb70cf2130ed73d3d9d5e
dittmar <dittmar.krall@matique.de>
2023-04-25 15:02:40 +0200

    'v0.1.9'

--- lib/omniauth-matique/version.rb
+++ lib/omniauth-matique/version.rb
@@ -1,7 +1,7 @@
 module OmniAuth
   module Matique
     VERSION = "0.1.9" # 2023-04-25
-    #  VERSION = "0.1.8" # 2021-06-23
+    # VERSION = "0.1.8" # 2021-06-23
     # VERSION = "0.1.7" # 2020-07-17
     # VERSION = "0.1.6" # 2020-04-27
     # VERSION = "0.1.5" # 2019-10-06
